a10e1102d8d0d3e360c75ba2c3737920f7cf7d8e
rharbert <rharbert981@comcast.net>
2022-05-17 13:54:01 -0400

    fixed lab request export

--- Commscope Schedule Database Solution/ScheduleDatabaseClassLibrary/Models/LabRequestModel.cs
+++ Commscope Schedule Database Solution/ScheduleDatabaseClassLibrary/Models/LabRequestModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ScheduleDatabaseClassLibrary.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,13 +9,21 @@ namespace ScheduleDatabaseClassLibrary.Models
 {
     public class LabRequestModel
     {
+        [ExcelExportProperty]
         public int ID { get; set; }
+        [ExcelExportProperty]
         public string LRID { get; set; }
+        [ExcelExportProperty]
         public string MSO { get; set; }
+        [ExcelExportProperty]
         public string Product { get; set; }
+        [ExcelExportProperty]
         public DateTime StartDate { get; set; }
+        [ExcelExportProperty]
         public DateTime EndDate { get; set; }
+        [ExcelExportProperty]
         public string Description { get; set; }
+        [ExcelExportProperty]
         public string Remarks { get; set; }
     }
 }
