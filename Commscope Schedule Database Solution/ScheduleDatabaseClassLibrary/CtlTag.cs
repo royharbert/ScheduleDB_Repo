cef13dddc94d0ebc27d0be577df9a6976c550745
Roy Harbert <rharbert981@comcast.net>
2022-12-08 10:21:35 -0500

    pre-pull

---
+++ Commscope Schedule Database Solution/ScheduleDatabaseClassLibrary/CtlTag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleDatabaseClassLibrary
+{
+    public class CtlTag
+    {
+        public bool InitialLock { get; set; }
+        public string TableColumn { get; set; }
+        public string ObjModel { get; set; }
+    }
+}
