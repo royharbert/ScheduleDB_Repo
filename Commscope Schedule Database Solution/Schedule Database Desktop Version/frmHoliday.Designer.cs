70d020a6463b9fe4123cff0b16254c2be9243450
Leia Davison <leia.davison@commscope.com>
2023-03-10 14:49:31 -0800

    pre pull commit

--- Commscope Schedule Database Solution/Schedule Database Desktop Version/frmHoliday.Designer.cs
+++ Commscope Schedule Database Solution/Schedule Database Desktop Version/frmHoliday.Designer.cs
@@ -38,13 +38,15 @@
             this.txtName = new System.Windows.Forms.TextBox();
             this.btnNewHoliday = new System.Windows.Forms.Button();
             this.btnAddNew = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             ((System.ComponentModel.ISupportInitialize)(this.dgvHolidays)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dgvHolidays
             // 
             this.dgvHolidays.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvHolidays.Location = new System.Drawing.Point(25, 41);
+            this.dgvHolidays.Location = new System.Drawing.Point(22, 41);
             this.dgvHolidays.Name = "dgvHolidays";
             this.dgvHolidays.RowHeadersWidth = 62;
             this.dgvHolidays.Size = new System.Drawing.Size(385, 339);
@@ -54,7 +56,8 @@
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(25, 400);
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.btnSave.Location = new System.Drawing.Point(22, 400);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(177, 39);
             this.btnSave.TabIndex = 1;
@@ -64,7 +67,8 @@
             // 
             // btnClose
             // 
-            this.btnClose.Location = new System.Drawing.Point(233, 400);
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.btnClose.Location = new System.Drawing.Point(230, 400);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(177, 39);
             this.btnClose.TabIndex = 2;
@@ -74,39 +78,49 @@
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(442, 105);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(13, 16);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.Size = new System.Drawing.Size(96, 17);
             this.label1.TabIndex = 3;
             this.label1.Text = "Holiday Name";
             // 
             // dtpHoliday
             // 
-            this.dtpHoliday.Location = new System.Drawing.Point(533, 140);
+            this.dtpHoliday.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.dtpHoliday.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.dtpHoliday.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpHoliday.Location = new System.Drawing.Point(115, 63);
             this.dtpHoliday.Name = "dtpHoliday";
-            this.dtpHoliday.Size = new System.Drawing.Size(200, 20);
+            this.dtpHoliday.Size = new System.Drawing.Size(161, 23);
             this.dtpHoliday.TabIndex = 4;
             // 
             // label2
             // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(442, 140);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(20, 66);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.Size = new System.Drawing.Size(89, 17);
             this.label2.TabIndex = 5;
             this.label2.Text = "Holiday Date";
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(532, 102);
+            this.txtName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtName.Location = new System.Drawing.Point(115, 13);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(200, 20);
+            this.txtName.Size = new System.Drawing.Size(161, 23);
             this.txtName.TabIndex = 6;
             // 
             // btnNewHoliday
             // 
-            this.btnNewHoliday.Location = new System.Drawing.Point(520, 206);
+            this.btnNewHoliday.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.btnNewHoliday.Location = new System.Drawing.Point(519, 251);
             this.btnNewHoliday.Name = "btnNewHoliday";
             this.btnNewHoliday.Size = new System.Drawing.Size(177, 39);
             this.btnNewHoliday.TabIndex = 7;
@@ -116,6 +130,7 @@
             // 
             // btnAddNew
             // 
+            this.btnAddNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
             this.btnAddNew.Location = new System.Drawing.Point(126, 455);
             this.btnAddNew.Name = "btnAddNew";
             this.btnAddNew.Size = new System.Drawing.Size(177, 39);
@@ -124,17 +139,32 @@
             this.btnAddNew.UseVisualStyleBackColor = true;
             this.btnAddNew.Click += new System.EventHandler(this.btnAddNew_Click);
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40.14337F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 59.85663F));
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.dtpHoliday, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtName, 1, 0);
+            this.tableLayoutPanel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(429, 131);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(279, 100);
+            this.tableLayoutPanel1.TabIndex = 9;
+            // 
             // frmHoliday
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(763, 521);
+            this.ClientSize = new System.Drawing.Size(803, 519);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.btnAddNew);
             this.Controls.Add(this.btnNewHoliday);
-            this.Controls.Add(this.txtName);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.dtpHoliday);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.dgvHolidays);
@@ -144,8 +174,9 @@
             this.Text = "Holiday List";
             this.Load += new System.EventHandler(this.frmHoliday_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvHolidays)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -160,5 +191,6 @@
         private System.Windows.Forms.TextBox txtName;
         private System.Windows.Forms.Button btnNewHoliday;
         private System.Windows.Forms.Button btnAddNew;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
--- Commscope Schedule Database Solution/Schedule Database Desktop Version/frmPassword.Designer.cs
+++ Commscope Schedule Database Solution/Schedule Database Desktop Version/frmPassword.Designer.cs
@@ -38,80 +38,107 @@
             this.label4 = new System.Windows.Forms.Label();
             this.btnChangePass = new System.Windows.Forms.Button();
             this.btnClose = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtUsername
             // 
-            this.txtUsername.Location = new System.Drawing.Point(148, 31);
+            this.txtUsername.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.txtUsername.Location = new System.Drawing.Point(141, 3);
             this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(300, 20);
+            this.txtUsername.Size = new System.Drawing.Size(318, 24);
             this.txtUsername.TabIndex = 0;
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(37, 34);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label1.Location = new System.Drawing.Point(95, 8);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.Size = new System.Drawing.Size(40, 18);
             this.label1.TabIndex = 1;
             this.label1.Text = "User";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // txtNewPass
             // 
-            this.txtNewPass.Location = new System.Drawing.Point(148, 107);
+            this.txtNewPass.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNewPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.txtNewPass.Location = new System.Drawing.Point(141, 73);
             this.txtNewPass.Name = "txtNewPass";
             this.txtNewPass.PasswordChar = '*';
-            this.txtNewPass.Size = new System.Drawing.Size(300, 20);
+            this.txtNewPass.Size = new System.Drawing.Size(318, 24);
             this.txtNewPass.TabIndex = 2;
             // 
             // txtCurrentPass
             // 
-            this.txtCurrentPass.Location = new System.Drawing.Point(148, 69);
+            this.txtCurrentPass.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtCurrentPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.txtCurrentPass.Location = new System.Drawing.Point(141, 38);
             this.txtCurrentPass.Name = "txtCurrentPass";
             this.txtCurrentPass.PasswordChar = '*';
-            this.txtCurrentPass.Size = new System.Drawing.Size(300, 20);
+            this.txtCurrentPass.Size = new System.Drawing.Size(318, 24);
             this.txtCurrentPass.TabIndex = 3;
             // 
             // txtConfirmPass
             // 
-            this.txtConfirmPass.Location = new System.Drawing.Point(148, 151);
+            this.txtConfirmPass.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtConfirmPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.txtConfirmPass.Location = new System.Drawing.Point(141, 108);
             this.txtConfirmPass.Name = "txtConfirmPass";
             this.txtConfirmPass.PasswordChar = '*';
-            this.txtConfirmPass.Size = new System.Drawing.Size(300, 20);
+            this.txtConfirmPass.Size = new System.Drawing.Size(318, 24);
             this.txtConfirmPass.TabIndex = 4;
             // 
             // label2
             // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(37, 79);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label2.Location = new System.Drawing.Point(7, 43);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.Size = new System.Drawing.Size(128, 18);
             this.label2.TabIndex = 5;
             this.label2.Text = "Current Password";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // label3
             // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(37, 117);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label3.Location = new System.Drawing.Point(26, 78);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.Size = new System.Drawing.Size(109, 18);
             this.label3.TabIndex = 6;
             this.label3.Text = "New Password";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // label4
             // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(37, 154);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label4.Location = new System.Drawing.Point(3, 113);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(91, 13);
+            this.label4.Size = new System.Drawing.Size(132, 18);
             this.label4.TabIndex = 7;
             this.label4.Text = "Confirm Password";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // btnChangePass
             // 
-            this.btnChangePass.Location = new System.Drawing.Point(103, 222);
+            this.btnChangePass.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.btnChangePass.Location = new System.Drawing.Point(31, 213);
             this.btnChangePass.Name = "btnChangePass";
-            this.btnChangePass.Size = new System.Drawing.Size(152, 23);
+            this.btnChangePass.Size = new System.Drawing.Size(213, 43);
             this.btnChangePass.TabIndex = 8;
             this.btnChangePass.Text = "Change Password";
             this.btnChangePass.UseVisualStyleBackColor = true;
@@ -119,33 +146,51 @@
             // 
             // btnClose
             // 
-            this.btnClose.Location = new System.Drawing.Point(278, 222);
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.btnClose.Location = new System.Drawing.Point(277, 213);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(170, 23);
+            this.btnClose.Size = new System.Drawing.Size(213, 43);
             this.btnClose.TabIndex = 9;
             this.btnClose.Text = "Close/Cancel";
             this.btnClose.UseVisualStyleBackColor = true;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 29.89011F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70.10989F));
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtConfirmPass, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.txtNewPass, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtCurrentPass, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtUsername, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(31, 44);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(462, 140);
+            this.tableLayoutPanel1.TabIndex = 10;
             // 
             // frmPassword
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(527, 281);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnChangePass);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtConfirmPass);
-            this.Controls.Add(this.txtCurrentPass);
-            this.Controls.Add(this.txtNewPass);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtUsername);
             this.Name = "frmPassword";
             this.Text = "Change Password";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -161,5 +206,6 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btnChangePass;
         private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
