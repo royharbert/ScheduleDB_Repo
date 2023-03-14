02abb3aaec11cc075573f790f7e93b1843288b43
Leia Davison <leia.davison@commscope.com>
2023-03-14 09:02:20 -0700

    pre pull commit

--- Commscope Schedule Database Solution/Schedule Database Desktop Version/frmHoliday.Designer.cs
+++ Commscope Schedule Database Solution/Schedule Database Desktop Version/frmHoliday.Designer.cs
@@ -46,10 +46,11 @@
             // dgvHolidays
             // 
             this.dgvHolidays.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvHolidays.Location = new System.Drawing.Point(22, 41);
+            this.dgvHolidays.Location = new System.Drawing.Point(16, 13);
+            this.dgvHolidays.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dgvHolidays.Name = "dgvHolidays";
             this.dgvHolidays.RowHeadersWidth = 62;
-            this.dgvHolidays.Size = new System.Drawing.Size(385, 339);
+            this.dgvHolidays.Size = new System.Drawing.Size(513, 417);
             this.dgvHolidays.TabIndex = 0;
             this.dgvHolidays.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvHolidays_CellClick);
             this.dgvHolidays.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvHolidays_CellEndEdit);
@@ -57,9 +58,10 @@
             // btnSave
             // 
             this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.btnSave.Location = new System.Drawing.Point(22, 400);
+            this.btnSave.Location = new System.Drawing.Point(13, 450);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(177, 39);
+            this.btnSave.Size = new System.Drawing.Size(236, 48);
             this.btnSave.TabIndex = 1;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
@@ -68,9 +70,10 @@
             // btnClose
             // 
             this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.btnClose.Location = new System.Drawing.Point(230, 400);
+            this.btnClose.Location = new System.Drawing.Point(290, 450);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(177, 39);
+            this.btnClose.Size = new System.Drawing.Size(236, 48);
             this.btnClose.TabIndex = 2;
             this.btnClose.Text = "Close";
             this.btnClose.UseVisualStyleBackColor = true;
@@ -81,7 +84,8 @@
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label1.Location = new System.Drawing.Point(13, 16);
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(96, 17);
             this.label1.TabIndex = 3;
@@ -91,10 +95,10 @@
             // 
             this.dtpHoliday.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.dtpHoliday.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.dtpHoliday.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtpHoliday.Location = new System.Drawing.Point(115, 63);
+            this.dtpHoliday.Location = new System.Drawing.Point(116, 80);
+            this.dtpHoliday.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dtpHoliday.Name = "dtpHoliday";
-            this.dtpHoliday.Size = new System.Drawing.Size(161, 23);
+            this.dtpHoliday.Size = new System.Drawing.Size(276, 23);
             this.dtpHoliday.TabIndex = 4;
             // 
             // label2
@@ -102,7 +106,8 @@
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label2.Location = new System.Drawing.Point(20, 66);
+            this.label2.Location = new System.Drawing.Point(19, 83);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(89, 17);
             this.label2.TabIndex = 5;
@@ -112,17 +117,19 @@
             // 
             this.txtName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.txtName.Location = new System.Drawing.Point(115, 13);
+            this.txtName.Location = new System.Drawing.Point(116, 19);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(161, 23);
+            this.txtName.Size = new System.Drawing.Size(276, 23);
             this.txtName.TabIndex = 6;
             // 
             // btnNewHoliday
             // 
             this.btnNewHoliday.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.btnNewHoliday.Location = new System.Drawing.Point(519, 251);
+            this.btnNewHoliday.Location = new System.Drawing.Point(640, 309);
+            this.btnNewHoliday.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnNewHoliday.Name = "btnNewHoliday";
-            this.btnNewHoliday.Size = new System.Drawing.Size(177, 39);
+            this.btnNewHoliday.Size = new System.Drawing.Size(215, 48);
             this.btnNewHoliday.TabIndex = 7;
             this.btnNewHoliday.Text = "Save New Holiday";
             this.btnNewHoliday.UseVisualStyleBackColor = true;
@@ -131,9 +138,10 @@
             // btnAddNew
             // 
             this.btnAddNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.btnAddNew.Location = new System.Drawing.Point(126, 455);
+            this.btnAddNew.Location = new System.Drawing.Point(148, 517);
+            this.btnAddNew.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnAddNew.Name = "btnAddNew";
-            this.btnAddNew.Size = new System.Drawing.Size(177, 39);
+            this.btnAddNew.Size = new System.Drawing.Size(236, 48);
             this.btnAddNew.TabIndex = 8;
             this.btnAddNew.Text = "Add New Holiday";
             this.btnAddNew.UseVisualStyleBackColor = true;
@@ -142,33 +150,36 @@
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40.14337F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 59.85663F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 28.45304F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 71.54696F));
             this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.dtpHoliday, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.txtName, 1, 0);
             this.tableLayoutPanel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(429, 131);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(551, 160);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(279, 100);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(396, 123);
             this.tableLayoutPanel1.TabIndex = 9;
             // 
             // frmHoliday
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(803, 519);
+            this.ClientSize = new System.Drawing.Size(977, 591);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.btnAddNew);
             this.Controls.Add(this.btnNewHoliday);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.dgvHolidays);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmHoliday";
             this.Tag = "";
             this.Text = "Holiday List";
--- Commscope Schedule Database Solution/ScheduleDatabaseClassLibrary/GeneralOps/ControlResize.cs
+++ Commscope Schedule Database Solution/ScheduleDatabaseClassLibrary/GeneralOps/ControlResize.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace ScheduleDatabaseClassLibrary.GeneralOps
 {
@@ -11,7 +13,16 @@ namespace ScheduleDatabaseClassLibrary.GeneralOps
     {
         public static void ResizeControl(Control ctl, int left, int width, int frm)
         {
-
+            originalFormSize = this.Size;
+            btnOriginalRect = new Rectangle()
         }
+
+        public static Rectangle btnOriginalRect;
+        public static Rectangle originalFormSize;
+        public static Rectangle txtOriginalRect;
+
+
+
+
     }
 }
